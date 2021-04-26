e1fa8d2c50d66c70e1d5d7fbb912780b18e317ec
Germain Pereira <pereira.germain@outlook.com>
2021-04-26 12:40:45 +0200

    Cleaned code

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
+// Imports
 const puppeteer = require("puppeteer");
 const dateFns = require("date-fns");
 
-function delay(timeout) {
+// Declare Functions
+
+function delayMs(timeout) {
   return new Promise((resolve) => {
     setTimeout(resolve, timeout);
   });
 }
 
-const today = new Date();
-
-const minimalSalaries = [
-  { startDate: new Date(2021, 01 - 1, 01), value: 1100 },
-  { startDate: new Date(2020, 02 - 1, 01), value: 1045 },
-  { startDate: new Date(2020, 01 - 1, 01), value: 1039 },
-  { startDate: new Date(2019, 01 - 1, 01), value: 998 },
-];
-
-debts = [];
-
-const initial_date = new Date(2019, 05 - 1, 15);
-let comparingDate = initial_date;
-const final_date = new Date(2021, 04 - 1, 16);
-
-console.log(initial_date, final_date);
-
-let finalDateIsNotReached = true;
-
-while (finalDateIsNotReached) {
+function addDebtInfo(minimalSalaries, debts, updatedComparingDate) {
   minimalSalaryInForce = minimalSalaries.find((minimalSalary) => {
-    wasInForce = dateFns.isAfter(comparingDate, minimalSalary.startDate);
+    wasInForce = dateFns.isAfter(updatedComparingDate, minimalSalary.startDate);
     return wasInForce;
   });
 
   debt = {
     label:
-      dateFns.format(comparingDate, "MM/yyyy") +
+      dateFns.format(updatedComparingDate, "MM/yyyy") +
       " - Principal Pensão Alimentícia - 11% do SM",
-    date: dateFns.format(comparingDate, "ddMMyyyy"),
+    date: dateFns.format(updatedComparingDate, "ddMMyyyy"),
     value: `${minimalSalaryInForce.value * (11 / 100)}`.replace(".", ","),
   };
 
   debts.push(debt);
-
-  comparingDate = dateFns.add(comparingDate, { months: 1 });
-  finalDateIsNotReached = dateFns.isBefore(comparingDate, final_date);
-  console.log(comparingDate, finalDateIsNotReached);
 }
 
-console.log(initial_date, final_date);
-console.log(debts);
+function addAllDebInfos(
+  minimalSalaries,
+  debts,
+  updatedComparingDate,
+  final_date
+) {
+  let isFinalDateNotReached = true;
+  while (isFinalDateNotReached) {
+    addDebtInfo(minimalSalaries, debts, updatedComparingDate);
 
-async function addLine(page, debt) {
+    updatedComparingDate = dateFns.add(updatedComparingDate, { months: 1 });
+    isFinalDateNotReached = dateFns.isBefore(updatedComparingDate, final_date);
+  }
+}
+
+async function launchBrowserInPage(pageToOpen) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(pageToOpen);
+
+  return page;
+}
+
+async function addDebtLineToPage(debt, page) {
   await page.type("#formRelatorivoA", debt.label);
   await page.focus("#formDataInicial_input");
   await page.waitForSelector("#formDataInicial_input", {
@@ -62,25 +62,66 @@ async function addLine(page, debt) {
   await page.click("#btnVincularValorAtualizacao");
 }
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(
-    "http://www8.tjmg.gov.br/cadej/pages/web/calculoSimples.xhtml"
-  );
-
+async function addDebtLinesToPageWithInterval(debts, page, interval) {
   for (debt of debts) {
-    await addLine(page, debt);
-    await delay(3000);
+    await addDebtLineToPage(debt, page);
+    await delayMs(interval);
   }
+}
 
-  await page.click("#btnCalcular");
-  await delay(3000);
+async function calculateDebts(buttonId, delay) {
+  await page.click(buttonId);
+  await delayMs(delay);
+}
 
+async function openTab(tabRef) {
   await Promise.all([
-    page.$eval("li a[href*='tabVisualizarCalculo']", (el) => el.click()),
+    page.$eval(tabRef, (el) => el.click()),
     page.waitForNavigation(),
   ]).catch((e) => console.log(e));
+}
 
-  await page.click("#j_idt206");
+// Initialize Variables
+const INTERVAL_BETWEEN_ACTIONS = 3000;
+const today = new Date();
+const initial_date = new Date(2019, 05 - 1, 15);
+let updatedComparingDate = initial_date;
+const final_date = new Date(2021, 04 - 1, 16);
+const TJMG_CALC_PAGE =
+  "http://www8.tjmg.gov.br/cadej/pages/web/calculoSimples.xhtml";
+const minimalSalaries = [
+  { startDate: new Date(2021, 01 - 1, 01), value: 1100 },
+  { startDate: new Date(2020, 02 - 1, 01), value: 1045 },
+  { startDate: new Date(2020, 01 - 1, 01), value: 1039 },
+  { startDate: new Date(2019, 01 - 1, 01), value: 998 },
+];
+debts = [];
+const CALC_BUTTON_ID = "#btnCalcular";
+const SHOW_CALC_TAB_REF = "li a[href*='tabVisualizarCalculo']";
+const DOWNLOAD_BUTTON_ID = "#j_idt206";
+
+/////////////////////////////////////////////////////
+////////////////                     ////////////////
+////////////////     Run Program     ////////////////
+////////////////                     ////////////////
+/////////////////////////////////////////////////////
+
+addAllDebInfos(minimalSalaries, debts, updatedComparingDate, final_date);
+
+(async () => {
+  const page = await launchBrowserInPage(TJMG_CALC_PAGE);
+
+  await addDebtLinesToPageWithInterval(debts, page, INTERVAL_BETWEEN_ACTIONS);
+
+  await calculateDebts(CALC_BUTTON_ID, INTERVAL_BETWEEN_ACTIONS);
+
+  await page.focus("#formDataInicial_input");
+  await page.waitForSelector("#formDataInicial_input", {
+    display: "block",
+  });
+  await page.type("#formDataInicial_input", debt.date);
+
+  await openTab(SHOW_CALC_TAB_REF);
+
+  await page.click(DOWNLOAD_BUTTON_ID);
 })();
